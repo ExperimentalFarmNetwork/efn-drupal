2c130a9fd3e33b381e459efb282479e5c8b65d64
onion <onion@riseup.net>
2016-12-11 22:36:24 -0500

    templates, js css for navbar, logo, front page banner

--- themes/minimus/js/skripto.js
+++ themes/minimus/js/skripto.js
@@ -1,4 +1,20 @@
 (function($, Drupal) {
     $(".block-group-operations button.btn-xs").removeClass('btn-xs btn-default').addClass('btn-lg btn-success');
 
+// resize navbar on scroll
+var navbar=$('header.navbar-default');
+    $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you wa
+ 
+      //nav bar to stick.  
+      console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 160) {
+      navbar.addClass('navbar-fixed-top');
+    }
+    if ($(window).scrollTop() < 161) {
+      navbar.removeClass('navbar-fixed-top');
+    }
+  });
+
 })(jQuery, Drupal);
