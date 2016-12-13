36ddc7c03b52b8772b4353fe526a4b73041b3601
onion <onion@riseup.net>
2016-12-13 14:13:35 -0500

    project banner config, twig and styles

--- themes/minimus/js/skripto.js
+++ themes/minimus/js/skripto.js
@@ -1,28 +1,39 @@
 (function($, Drupal) {
+    // change bootstrap button for add project update
     $(".block-group-operations button.btn-xs").removeClass('btn-xs btn-default').addClass('btn-lg btn-success');
 
-// resize navbar on scroll
-var navbar=$('header.navbar-default');
-    $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you wa
- 
-      //nav bar to stick.  
-      console.log($(window).scrollTop());
-    if ($(window).scrollTop() > 160) {
-      navbar.addClass('navbar-fixed-top');
-    }
-    if ($(window).scrollTop() < 161) {
-      navbar.removeClass('navbar-fixed-top');
-    }
-  });
-$("button.toggleVerbose").click(function(event) {
-    $('.path-group .region-content .field').toggleClass('toggleVFields','1000');
-});
+    // resize navbar on scroll
+    var navbar=$('header.navbar-default');
+        $(window).scroll(function () {
+        if ($(window).scrollTop() > 160) {
+          navbar.addClass('navbar-fixed-top');
+        }
+        if ($(window).scrollTop() < 161) {
+          navbar.removeClass('navbar-fixed-top');
+        }
+      });
 
-$("textarea.form-textarea").attr("placeholder", "Comment here ...").click(function(event) {
-    $(this).attr('placeholder', 'yes do it');
-    $('.js-form-submit').css('display', 'block');
-});;
+    $("button.toggleVerbose").click(function(event) {
+        $('.path-group .region-content .field').toggleClass('toggleVFields','1000');
+    });
+
+    $("textarea.form-textarea").attr("placeholder", "Comment here ...").click(function(event) {
+        $(this).attr('placeholder', 'yes do it');
+        $('.js-form-submit').css('display', 'block');
+    });
+
+    // Put header image in background of group pages
+    var headerImg = $(".path-group .field--name-field-project-image img").attr('src');
+    $("body.path-group").css({
+        'background': 'url('+headerImg+')',
+        'background-size':' contain',
+        'background-repeat':'no-repeat'
+    });
+
+    // set project update inititial background offset
+    var hdrHt=$("body.path-group .navbar-default").height(),
+        hdrMargin=hdrHt+100;
+    $("body.path-group").css('background-position-y', hdrMargin+'px');
+    console.log(hdrMargin);
 
 })(jQuery, Drupal);
