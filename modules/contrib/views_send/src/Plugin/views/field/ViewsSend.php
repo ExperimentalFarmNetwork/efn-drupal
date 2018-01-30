ca2880334c160963a335f0edacef99d0732e3ae2
onion <onion@riseup.net>
2018-01-30 12:17:54 -0500

    adding views send

---
+++ modules/contrib/views_send/src/Plugin/views/field/ViewsSend.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Drupal\views_send\Plugin\views\field;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\system\Plugin\views\field\BulkForm;;
+use Drupal\views\ResultRow;
+
+/**
+ * Defines a simple send mass mail form element.
+ *
+ * @ViewsField("views_send_bulk_form")
+ */
+class ViewsSend extends BulkForm {
+
+  /**
+   * Overrides \Drupal\system\Plugin\views\field\BulkForm::buildOptionsForm().
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+    // Hide options that are irrelevant.
+    $form['element_label_colon']['#access'] = FALSE;
+    $form['exclude']['#access'] = FALSE;
+    $form['alter']['#access'] = FALSE;
+    $form['empty_field_behavior']['#access'] = FALSE;
+    // Hide the available actions configuration as we haven't defined a bulk action to select from.
+    $form['action_title']['#access'] = FALSE;
+    $form['include_exclude']['#access'] = FALSE;
+    $form['selected_actions']['#access'] = FALSE;
+  }
+
+  /**
+   * Overrides \Drupal\system\Plugin\views\field\BulkForm::viewsForm(). 
+   */
+  function viewsForm(&$form, FormStateInterface $form_state) {
+    parent::viewsForm($form, $form_state);
+
+    // The view is empty, abort.
+    if (empty($this->view->result)) {
+      return;
+    }
+
+    // Add the custom CSS for all steps of the form.
+    $form['#attached']['library'][] = 'views_send/views_send.form';
+
+    // Remove standard header which is used to select action
+    unset($form['header']);
+
+    $step = $form_state->get('step');
+    if ($step == 'views_form_views_form') {
+      $form['actions']['submit']['#value'] = $this->t('Next');
+      $form['#prefix'] = '<div class="views-send-selection-form">';
+      $form['#suffix'] = '</div>';
+    }
+    else {
+      // Hide the normal output from the view
+      unset($form['output']);
+      $step($form, $form_state, $this->view);
+    }
+  }
+
+  /**
+   * Overrides \Drupal\system\Plugin\views\field\BulkForm::getBulkOptions(). 
+   */
+  protected function getBulkOptions($filtered = TRUE) {
+    return [];
+  }
+
+  /**
+   * Overrides \Drupal\system\Plugin\views\field\BulkForm::viewsFormSubmit(). 
+   */
+  function viewsFormSubmit(&$form, FormStateInterface $form_state) {
+    switch ($form_state->get('step')) {
+      case 'views_form_views_form':
+        $field_name = $this->options['id'];
+        $selection = array_filter($form_state->getValue($field_name));
+        $form_state->set('selection', array_keys($selection));
+
+        $form_state->set('step', 'views_send_config_form');
+        $form_state->setRebuild(TRUE);
+        break;
+
+      case 'views_send_config_form':
+        $display = $form['display']['#value'];
+        $config = \Drupal::configFactory()->getEditable('views_send.user_settings');
+        $config_basekey = $display . '.uid:' . \Drupal::currentUser()->id();
+        $form_state_values = $form_state->getValues();
+        if ($form_state->getValue('views_send_remember')) {
+          foreach ($form_state_values as $key => $value) {
+            $key = ($key == 'format') ? 'views_send_message_format' : $key;
+            if (substr($key, 0, 11) == 'views_send_') {
+              $config->set($config_basekey . '.' . substr($key,11), $value);
+            }
+          }
+          $config->save();
+        } else {
+          $config->clear($config_basekey);
+          $config->save();
+        }
+        $form_state->set('configuration', $form_state_values);
+
+        // If a file was uploaded, process it.
+        if (VIEWS_SEND_MIMEMAIL && \Drupal::currentUser()->hasPermission('attachments with views_send') &&
+            isset($_FILES['files']) && is_uploaded_file($_FILES['files']['tmp_name']['views_send_attachments'])) {
+          // attempt to save the uploaded file
+          $dir = file_default_scheme() . '://views_send_attachments';
+          file_prepare_directory($dir, FILE_CREATE_DIRECTORY);
+          $file = file_save_upload('views_send_attachments', $form_state, array(), $dir);
+          // set error if file was not uploaded
+          if (!$file) {
+            $form_state->setErrorByName('views_send_attachment', $this->t('Error uploading file.'));
+          }
+          else {
+            // set files to form_state, to process when form is submitted
+            // @todo: when we add a multifile formfield then loop through to add each file to attachments array
+            $form_state->set(array('configuration', 'views_send_attachments'), (array)$file);
+          }
+        }
+
+        $form_state->set('step', 'views_send_confirm_form');
+        $form_state->setRebuild(TRUE);
+        break;
+
+      case 'views_send_confirm_form':
+
+        // Queue the email for sending.
+        views_send_queue_mail($form_state->get('configuration'), $form_state->get('selection'), $this->view);
+
+        // Redirect. (FIXME - query...)
+        $query = UrlHelper::filterQueryParameters($_GET, array('q'));
+        $form_state->setRedirectUrl($this->view->getUrl());
+        break;
+    }
+  }
+  
+  /**
+   * Overrides \Drupal\system\Plugin\views\field\BulkForm::::viewsFormValidate(). 
+   */
+  function viewsFormValidate(&$form, FormStateInterface $form_state) {
+    if ($form_state->get('step') != 'views_form_views_form') {
+      return;
+    }
+    // Only the first initial form is handled here.
+    $field_name = $this->options['id'];
+    $selection = array_filter($form_state->getValue($field_name));
+
+    if (empty($selection)) {
+      $form_state->setErrorByName($field_name, $this->t('Please select at least one item.'));
+    }
+  }
+}
---
+++ modules/contrib/views_send/src/SettingsForm.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\views_send;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure update settings for this site.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'views_send_settingsform';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'views_send.settings',
+    ];
+  }
+
+  /**
+   * Implements \Drupal\Core\Form\FormInterface::buildForm().
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('views_send.settings');
+
+    $throttle_values = array(1, 10, 20, 30, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000);
+    $throttle = array_combine($throttle_values, $throttle_values);
+    array_unshift($throttle, $this->t('Unlimited'));
+
+    $throttle_desc = $this->t('Sets the numbers of messages sent per cron run. Failure to send will also be counted. Cron execution must not exceed the PHP maximum execution time of %max seconds.',
+      array('%max' => ini_get('max_execution_time')));
+    if (\Drupal::moduleHandler()->moduleExists('dblog')) {
+      $throttle_desc .= ' ' . $this->t('You find the time spent to send e-mails in the <a href="@dblog-url">recent log messages</a>.', array('@dblog-url' => $this->url('dblog.overview')));
+    }
+    $form['throttle'] = array(
+      '#type' => 'select',
+      '#title' => $this->t('Cron throttle'),
+      '#options' => $throttle,
+      '#default_value' => $config->get('throttle'),
+      '#description' => $throttle_desc,
+    );
+
+    $form['spool_expire'] = array(
+      '#type' => 'select',
+      '#title' => $this->t('Mail spool expiration'),
+      '#options' => array(0 => $this->t('Immediate'), 1 => $this->t('1 day'), 7 => $this->t('1 week'), 14 => $this->t('2 weeks')),
+      '#default_value' => $config->get('spool_expire'),
+      '#description' => $this->t('E-mails are spooled. How long must messages be retained in the spool after successfull sending.'),
+    );
+
+    $form['debug'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Log e-mails'),
+      '#default_value' => $config->get('debug'),
+      '#description' => $this->t('When checked all outgoing mesages are logged in the system log. A logged e-mail does not guarantee that it is send or will be delivered. It only indicates that a message is send to the PHP mail() function. No status information is available of delivery by the PHP mail() function.'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * Implements \Drupal\Core\Form\FormInterface::validateForm().
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * Implements \Drupal\Core\Form\FormInterface::submitForm().
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('views_send.settings');
+
+    $config
+      ->set('throttle', $form_state->getValue('throttle'))
+      ->set('spool_expire', $form_state->getValue('spool_expire'))
+      ->set('debug', $form_state->getValue('debug'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
