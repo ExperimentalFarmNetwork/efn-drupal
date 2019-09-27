5383beb29aa7d98b53464bf503998c3b61404cf0
Dave Onion <onion@radicante.media>
2019-09-27 15:53:07 -0400

    removing field_group module because deprecated

--- modules/contrib/geocoder/src/Plugin/Geocoder/Provider/ArcGISOnline.php
+++ modules/contrib/geocoder/src/Plugin/Geocoder/Provider/ArcGISOnline.php
@@ -8,7 +8,7 @@ use Drupal\geocoder\ConfigurableProviderUsingHandlerWithAdapterBase;
  * Provides an ArcGISOnline geocoder provider plugin.
  *
  * @GeocoderProvider(
- *   id = "Arcgisonline",
+ *   id = "arcgisonline",
  *   name = "ArcGISOnline",
  *   handler = "\Geocoder\Provider\ArcGISOnline\ArcGISOnline",
  *   arguments = {
